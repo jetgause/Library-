2a0f35acbd794be1ca13390360ee6270d9454c53
jetgause <jetgause@gmail.com>
2025-12-09 15:05:54 -0800

    Create pulse/__init__.py with PULSE Tool Factory and Economics System v2.0.0

---
+++ pulse/__init__.py
@@ -0,0 +1,12 @@
+"""
+PULSE Tool Factory and Economics System
+Version 2.0.0
+"""
+
+__version__ = "2.0.0"
+__author__ = "jetgause"
+
+from pulse.tool_factory import ToolFactory
+from pulse.economics import ValueEngine
+
+__all__ = ["ToolFactory", "ValueEngine"]
